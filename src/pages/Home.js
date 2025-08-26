33fb585134113dbb3c562879531be6242f0f31ca
rebeccakibisu <coursecpd@gmail.com>
2025-08-26 13:44:41 +0300

    Update dependencies and implement routing with user profile functionality
    - Updated React and React Router dependencies in package.json and package-lock.json.
    - Added routing for Home, About, Login, and UserProfile pages.
    - Implemented UserCard component with profile link.
    - Created NavBar component for navigation.
    - Added ErrorPage component for handling routing errors.
    - Updated Home component to fetch users from the new server port.
    - Created Login and About pages with NavBar integration.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 import UserCard from "../components/UserCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
   const [users, setUsers] = useState([])
 
   useEffect(() =>{
-    fetch("http://localhost:4000/users")
+    fetch("http://localhost:3000/users")
       .then(r => r.json())
       .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
+      .catch(error => console.error(error))
+  }, [])
+
   const userList = users.map(user =>{
     return <UserCard key={user.id} user={user}/>
   });
@@ -18,7 +19,7 @@ function Home() {
   return (
     <>
       <header>
-        {/* place NavBar here */}
+        <NavBar />
       </header>
       <main>
         <h1>Home!</h1>
